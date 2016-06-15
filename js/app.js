16e5bf5b3ae998bb8440381f490a75b312ed945b
Erick-Munene <munenewahome@gmail.com>
2016-06-15 23:06:18 +0300

    styled the contact section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 $(document).foundation() 
 
-$('#element').foundation('toggleMenu');
+$(document).ready(function(){
+    $(".button-nav a").click(function(){
+        $(".overlay").fadeToggle(200);
+       $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+});
+$('.overlay').on('click', function(){
+//    $(".overlay").fadeToggle(200);   
+    $(".button-nav a").toggleClass('btn-open').toggleClass('btn-close');
+    open = false;
+});
